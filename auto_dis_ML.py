4a30e643d71b0ae3d7db51414af0eb590637c1df
yee23456 <b10805025@ctbc.edu.tw>
2022-04-17 19:18:18 +0800

    2022/4/17

--- auto_dis_ML.py
+++ auto_dis_ML.py
@@ -1,16 +1,18 @@
 import os
+import jieba
 import joblib
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,confusion_matrix
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import classification_report,confusion_matrix
+
 #%%
 stop_word = ['h','▲','記者','▼','【','►',' ','《','[']
 def input_file(name):
-    path = f"C:/Users/Paul/Desktop/貼標/{name}" #資料夾目錄
+    path = f"C:/Users/Paul/Documents/github/news/{name}" #資料夾目錄
     files= os.listdir(path) #得到資料夾下的所有檔名稱
     word = []
     word_text = ''
@@ -43,12 +45,8 @@ data = pd.DataFrame(domestic+foreign,columns=['file'])
 data['lable']= '國內'
 data['lable'][d_num:] = '國外'
 data.drop(data[data['file'] == ''].index,inplace=True)
-#%%
 
-print(data.iloc[175:225])
 #%%
-
-import jieba
 data['file_Participle'] = data['file'].apply(lambda i:jieba.cut(i) )
 data['file_Participle'] =[' '.join(i) for i in data['file_Participle']]
 
@@ -62,13 +60,10 @@ xtrain, xvalid, ytrain, yvalid = train_test_split(data.file_Participle.values, y
                                                   test_size=0.2, shuffle=True)
 
 print (xtrain.shape)
-# print (ytrain.shape)
 print (xvalid.shape)
 
 
 #%%
-
-
 def multiclass_logloss(actual, predicted, eps=1e-15):
     """对数损失度量（Logarithmic Loss  Metric）的多分类版本。
     :param actual: 包含actual target classes的数组
@@ -86,16 +81,14 @@ def multiclass_logloss(actual, predicted, eps=1e-15):
     vsota = np.sum(actual * np.log(clip))
     return -1.0 / rows * vsota
 
-
 #%%
-stwlist=[line.strip() for line in open('C:/Users/Paul/Desktop/貼標/stopwords0.txt',
+stwlist=[line.strip() for line in open('C:/Users/Paul/Documents/github/news/stopwords0.txt',
 'r',encoding='utf-8').readlines()]
 
 ctv = CountVectorizer(min_df=3,
                       max_df=0.5,
                       ngram_range=(1,2),
                       stop_words = stwlist)
-
 # 使用Count Vectorizer来fit训练集和测试集（半监督学习）
 ctv.fit(list(xtrain) + list(xvalid))
 xtrain_ctv =  ctv.transform(xtrain) 
@@ -103,8 +96,9 @@ xvalid_ctv = ctv.transform(xvalid)
 
 #利用提取的word counts特征来fit一个简单的Logistic Regression 
 # clf = RandomForestClassifier(n_estimators=50,max_features="sqrt",oob_score=True,random_state=3)
-clf = LogisticRegression(C=1.0,solver='lbfgs',multi_class='multinomial') #test_size=0.2
 # clf = MLPClassifier(solver='lbfgs',hidden_layer_sizes=(8,3), random_state=1)
+
+clf = LogisticRegression(C=1.0,solver='lbfgs',multi_class='multinomial') #test_size=0.2
 clf.fit(xtrain_ctv, ytrain)
 predictions = clf.predict_proba(xvalid_ctv)
 
@@ -114,19 +108,20 @@ print(confusion_matrix(yvalid,predictions_2))
 print(classification_report(yvalid,predictions_2))
 print ("logloss: %0.3f " % multiclass_logloss(yvalid, predictions))
 
-
 #%%
 joblib.dump(clf,'clf.pkl')
 joblib.dump(ctv,'ctv.pkl')
+
 #%%
 #讀取時分小標
-
 import joblib
 import pandas as pd
 stop_word = ['h','▲','記者','▼','【','►',' ','《','[']
-path = "C:/Users/Paul/Desktop/貼標/測試" #資料夾目錄
+path = "C:/Users/Paul/Documents/github/news/測試" #資料夾目錄
 files= os.listdir(path) #得到資料夾下的所有檔名稱
 word = []
+# name = []
+# word_num = []
 word_text = ''
 for file in files: #遍歷資料夾
     if not os.path.isdir(file): #判斷是否是資料夾，不是資料夾才開啟
@@ -146,25 +141,34 @@ for file in files: #遍歷資料夾
         
         elif line[0][0] in stop_word:
             continue
+        
         word_text+=i.rstrip()
 
-print(len(word))
-#%%
+    # a = len(word)
+    # word_num.append(a)
+    # name.append(file.replace('.txt',''))
+# print(word_num)
+# print(name)
 
-clf2 = joblib.load('C:/Users/Paul/Desktop/貼標/clf.pkl')
-ctv2 = joblib.load('C:/Users/Paul/Desktop/貼標/ctv.pkl')
+#%%
+clf2 = joblib.load('C:/Users/Paul/Documents/github/news/clf.pkl')
+ctv2 = joblib.load('C:/Users/Paul/Documents/github/news/ctv.pkl')
 df = pd.DataFrame(word,columns = ['file'])
 df['file_Participle'] = df['file'].apply(lambda i:jieba.cut(i) )
 df['file_Participle'] =[' '.join(i) for i in df['file_Participle']]
 df['test_dis'] = df['file_Participle'] .apply(lambda x : clf2.predict(ctv2.transform([x])))
 df['分類項目'] = df['test_dis'].apply(lambda x: '國內' if x == 0 else '國外')
-print(df.head())
+# df['小標'] = '0'
+# def name_dis(num,asd):
+#     df['小標'][0:num+1] = asd
+
+# for i in range(len(name)):
+#     name_dis(word_num[::-1][i],name[::-1][i])
 
 #%%
-
 #編寫時分小標存檔
 def dis(dis_name,sub):    
-    path = f'C:/Users/Paul/Desktop/貼標/{dis_name}/2022_4_13.txt'
+    path = f'C:/Users/Paul/Documents/github/news/{dis_name}/2022_4_13.txt'
     f = open(path, 'w',encoding=('UTF-8'))
     for i in range(len(df)):
         if df.分類項目.iloc[i] == sub:
@@ -172,8 +176,7 @@ def dis(dis_name,sub):
             f.writelines('\n\n')
         else:
             pass
-    f.close
-    
+    f.close()
     
 dis('domestic','國內')
 dis('foreign','國外')
